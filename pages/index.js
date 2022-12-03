5f704ff0713b6e62eda81e89ed2e4c34e1613c45
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-12-02 23:17:51 -0800

    implemented facebook auth using nextauth

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import { getSession } from "next-auth/react"
+import Login from '../components/Login'
 
 
-export default function Home() {
+
+export default function Home({session}) {
+  if(!session){return <Login/>}
   return (
     <div >
       <Head>
@@ -14,4 +18,14 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session,
+    },
+  }
 }
