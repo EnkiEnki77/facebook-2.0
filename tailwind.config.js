92960d0423303091bd8493b6ac025bb5c937f3c6
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-12-04 19:57:04 -0800

    created inputbox component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  mode: "jit",
+  
   theme: {
     extend: {},
   },
